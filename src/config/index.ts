7ee33bac863dff9fa92e17df95ae87d05e2da484
Syam <hey@syam.dev>
2021-01-20 14:25:09 +0530

    added watchman , config module and postgres db integration

---
+++ src/config/index.ts
@@ -0,0 +1,35 @@
+import { Module, DynamicModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+config();
+
+import * as path from 'path';
+@Module({
+  providers: [],
+})
+export class ConfigModule {
+  static forRoot(database = process.env.DB_CONFIG_NAME): DynamicModule {
+    const ePath = path.join(__dirname, '../**/*.entity{.ts,.js}');
+    const mPath = path.join(__dirname, '../**/migrations/*{.ts,.js}');
+    const provider = TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      migrationsRun: true,
+      database: process.env.DB_NAME,
+      entities: [ePath],
+      migrations: [mPath],
+      synchronize: false,
+      autoLoadEntities: true,
+      logging: ['query', 'error', 'schema', 'warn', 'info', 'log'],
+    });
+    return {
+      module: ConfigModule,
+      imports: [provider],
+      // providers: [provider] ,
+      exports: [provider],
+    };
+  }
+}
