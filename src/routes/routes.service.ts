83e907b5322b6a317d31ede1a6fd383dbe586e10
unknown <psjishnu13@gmail.com>
2021-01-30 00:11:36 +0530

    added route schema

---
+++ src/routes/routes.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateRouteDto } from './dto/create-route.dto';
+import { UpdateRouteDto } from './dto/update-route.dto';
+
+@Injectable()
+export class RoutesService {
+  create(createRouteDto: CreateRouteDto) {
+    return 'This action adds a new route';
+  }
+
+  findAll() {
+    return `This action returns all routes`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} route`;
+  }
+
+  update(id: number, updateRouteDto: UpdateRouteDto) {
+    return `This action updates a #${id} route`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} route`;
+  }
+}
