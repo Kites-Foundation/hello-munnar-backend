83f55c49908f770a2aa4ed3288728867df1b33d4
Jishnu P Sreekumar <psjishnu13@gmail.com>
2021-01-30 10:21:16 +0530

    added update type

--- src/facilities/dto/updateType.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class updateTypeDTO {
-  @ApiProperty({ type: 'string' })
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-}
