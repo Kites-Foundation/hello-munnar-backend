1387497032282ee255dbcba93cc770fd731c6f43
Sreeraj <sreerajmp1996@gmail.com.com>
2021-01-30 22:17:15 +0530

    Add destination api

--- src/destinations/destinations.service.ts
+++ src/destinations/destinations.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException, HttpException, HttpStatus} from '@nestjs/common';
+import { AddDestinationDto } from './dto/addDestination.dto';
+import {InjectRepository} from '@nestjs/typeorm';
+import {DestinationRepository} from 'src/destinations/destination.repository'
 
 @Injectable()
-export class DestinationsService {}
+export class DestinationsService {
+    constructor(  
+    @InjectRepository(DestinationRepository)
+    private DestinationRepository : DestinationRepository){}
+
+    async addDestination(AddDestinationDto:AddDestinationDto):Promise <any>{
+        console.log("log:",AddDestinationDto)
+        return this.DestinationRepository.addDestination(AddDestinationDto);
+    }
+}
