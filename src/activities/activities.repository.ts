b339357d7aa3da5594ef249a2188fbf6c994ba38
KIRAN <kiranmurali93@gmail.com>
2021-01-30 19:18:18 +0530

    Update activities.repository.ts

--- src/activities/activities.repository.ts
+++ src/activities/activities.repository.ts
@@ -4,16 +4,16 @@ import Activities from './entities/activity.entity'
 @EntityRepository(Activities)
 export class ActivityRepository extends Repository<Activities>{
     async addActivity(addActivityDto: any): Promise <any>{
-        const {name, type, destination, description,cost, status, timeRange, bookingUrl, imageUrl} = addActivityDto;
+        const {name, type, destination, description,cost, status, time_range, booking_url, image_url} = addActivityDto;
         const activity = new Activities()
         activity.name = name
         activity.type = type
         activity.destination = destination
         activity.description = description
         activity.cost = cost
-        activity.bookingUrl = bookingUrl
-        activity.timeRange = timeRange
-        activity.imageUrl = imageUrl
+        activity.booking_url = booking_url
+        activity.time_range = time_range
+        activity.image_url = image_url
         activity.status = status
         console.log(activity)
         await activity.save()
