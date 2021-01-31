21efe3c869c03b389cdc46d75df24e7be78b5e58
kiranmurali93 <kiranmurali93@gmail.com>
2021-01-31 12:29:42 +0530

    comment added to activities.controllers.ts

--- src/activities/activities.controller.ts
+++ src/activities/activities.controller.ts
@@ -13,6 +13,7 @@ import { CreateActivityDto } from './dto/index.dto'
 export class ActivitiesController {
   constructor(private activitiesService: ActivitiesService) {}
   
+  // post request addActivity
   @Post('addActivity')
     createActivity(@Request() req:any,
     @Body() body:CreateActivityDto){
